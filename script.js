8fb131cd4046b69ce05389a6f4ea5f9dd2546383
Marcelo do nascimento mayer <107888086+MarceloMayer@users.noreply.github.com>
2022-10-24 16:41:27 -0300

    correções de caminhos

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ indicador.innerText = '?'
 
 //imagem
 let img = document.getElementById('img-dado')
-img.src = '/curso youtube/aula 16/img/0.png'
+img.src = 'img/0.png'
 
 //audio
 let audio = new Audio('/curso youtube/aula 16/som/dado-rolando.mp3')
@@ -21,7 +21,7 @@ function sortear() {
     audio.play()
 
     //resetar img e indicador, efeito de jogar o dado
-    img.src = '/curso youtube/aula 16/img/0.png'
+    img.src = 'img/0.png'
     indicador.innerText = '?'
 
     //sumir com o botao
@@ -36,17 +36,17 @@ function sortear() {
 
         //lógica da imagem
         if (n == 1) {
-            img.src = '/curso youtube/aula 16/img/1.png'
+            img.src = 'img/1.png'
         } else if (n == 2) {
-            img.src = '/curso youtube/aula 16/img/2.png'
+            img.src = 'img/2.png'
         } else if (n == 3) {
-            img.src = '/curso youtube/aula 16/img/3.png'
+            img.src = 'img/3.png'
         } else if (n == 4) {
-            img.src = '/curso youtube/aula 16/img/4.png'
+            img.src = 'img/4.png'
         } else if (n == 5) {
-            img.src = '/curso youtube/aula 16/img/5.png'
+            img.src = 'img/5.png'
         } else if (n == 6) {
-            img.src = '/curso youtube/aula 16/img/6.png'
+            img.src = 'img/6.png'
         }
 
         //fazer o botao reaparecer
